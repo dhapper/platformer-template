58aeccde3a96f1627e3279aa78f82dd7424ca4bb
dhapper <dhaarshan.p@gmail.com>
2025-11-18 16:35:04 -0500

    Initial commit

---
+++ Platformer_Game/src/main/GamePanel.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+
+	public GamePanel() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.drawRect(100, 100, 100, 100);
+		g.fillRect(200, 100, 100, 100);
+	}
+	
+}
